9577b4559644430fac7f947e6c2e97ac7fadbfbc
Rafael Moraes <contact@moraesrafael.com>
2012-02-06 19:39:04 -0200

    added dictionary range to config

--- app/models/dictionary.rb
+++ app/models/dictionary.rb
@@ -1,6 +1,6 @@
 class Dictionary
-  @@alphabet = (10241..10495).to_a
-  @@base = @@alphabet.length
+  @@alphabet = []
+  @@base = 0
 
   def self.word_for number
     word = [].tap do |d|
@@ -16,5 +16,15 @@ class Dictionary
     number = 0
     word.unpack('U*').map { |j| @@alphabet.index j }.reverse.each_with_index { |j,k| number += (@@base ** k) * j }
     number
+  end
+
+  def self.configure(configuration)
+    configuration['include'].each_slice(2).to_a.each do |j|
+      @@alphabet += (j[0]..j[1]).to_a
+    end
+    configuration['exclude'].each_slice(2).to_a.each do |j|
+      @@alphabet -= (j[0]..j[1]).to_a
+    end
+    @@base = @@alphabet.length
   end
 end
