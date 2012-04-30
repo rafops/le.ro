21184cb6833d1b9cb0e3d6b5c829eb1708c24195
Rafael Moraes <contact@moraesrafael.com>
2012-04-30 18:31:57 -0300

    added more chars

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,8 +3,9 @@ class HomeController < ApplicationController
 
   def index
     # http://www.alanwood.net/unicode/miscellaneous_symbols.html
-    @chars = [ 9728, 9730, 9733, 9742, 9749, 9752, 9760, 9762, 9756, 9773,
-               9774, 9786, 9793, 9822, 9827, 9829, 9830, 9835 ]
+    @chars = [ 9728, 9729, 9730, 9731, 9732, 9733, 9742, 9749, 9752, 9756,
+               9760, 9762, 9763, 9765, 9773, 9774, 9786, 9793, 9822, 9824,
+               9827, 9829, 9835, 9851, 9875, 9880, 9881 ]
     @shortening = Shortening.new
   end
 end
